f9685c7f46394338c0fdacec4aa7bd1258179ee0
zouf <matt@tigerstonelabs.com>
2014-02-14 09:56:46 -0500

    cast to float

--- src/mult.py
+++ src/mult.py
@@ -1,13 +1,13 @@
 def multiply(a,b):
-  return a*b
+  return float(a)*float(b)
 
 def divide(a,b):
   if b:
-    return a/b
+    return float(a)/float(b)
   return None
 
 def add(a,b):
-  return a+b
+  return float(a)+float(b)
 
 def sub(a,b):
-  return a-b
+  return float(a)-float(b)
